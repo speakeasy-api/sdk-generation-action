490e6eb7b074c28e3cd706fe05abd547705aab6c
Tristan Cartledge <tristan@speakeasyapi.dev>
2022-11-04 11:57:35 +0000

    feat: clone repo

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 func main() {
@@ -14,5 +19,30 @@ func main() {
 	fmt.Println("Docker Container ENV")
 	for _, env := range os.Environ() {
 		fmt.Println(env)
+	}
+
+	accessToken := os.Getenv("INPUT_GITHUB-ACCESS-TOKEN") // TODO validate
+
+	if accessToken == "" {
+		log.Fatal("No access token provided")
+	}
+
+	githubURL := os.Getenv("GITHUB_SERVER_URL")
+	githubRepoLocation := os.Getenv("GITHUB_REPOSITORY")
+
+	repoPath := path.Join(githubURL, githubRepoLocation)
+
+	log.Println("Cloning repo: ", repoPath)
+
+	_, err := git.PlainClone("/repo", false, &git.CloneOptions{
+		URL:      repoPath,
+		Progress: os.Stdout,
+		Auth: &http.BasicAuth{
+			Username: "gen", // yes, this can be anything except an empty string
+			Password: accessToken,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
