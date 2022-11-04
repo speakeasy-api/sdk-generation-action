9ebdd471e3ffc108d518b1e2a33fdabb481135a1
Tristan Cartledge <tristan@speakeasyapi.dev>
2022-11-04 11:19:01 +0000

    fix: update env var names

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Getenv("INPUT_SPEAKEASY-VERSION"))
-	fmt.Println(os.Getenv("INPUT_OPENAPI-DOC-LOCATION"))
-	fmt.Println(os.Getenv("INPUT_GITHUB-ACCESS-TOKEN"))
-	fmt.Println(os.Getenv("INPUT_LANGUAGES"))
+	fmt.Println("speakeasy-version: ", os.Getenv("INPUT_SPEAKEASY-VERSION"))
+	fmt.Println("openapi-doc-location: ", os.Getenv("INPUT_OPENAPI-DOC-LOCATION"))
+	fmt.Println("github-access-token: ", os.Getenv("INPUT_GITHUB-ACCESS-TOKEN"))
+	fmt.Println("languages: ", os.Getenv("INPUT_LANGUAGES"))
+
+	fmt.Println("Docker Container ENV")
+	for _, env := range os.Environ() {
+		fmt.Println(env)
+	}
 }
