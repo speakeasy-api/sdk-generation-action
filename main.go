e32d8161a98ad5be2c63972689205aad05219ee4
Tristan Cartledge <tristan@speakeasyapi.dev>
2022-11-04 11:16:48 +0000

    fix: env names

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Getenv("INPUT_SPEAKEASY_VERSION"))
-	fmt.Println(os.Getenv("INPUT_OPENAPI_DOC_LOCATION"))
-	fmt.Println(os.Getenv("INPUT_GITHUB_ACCESS_TOKEN"))
+	fmt.Println(os.Getenv("INPUT_SPEAKEASY-VERSION"))
+	fmt.Println(os.Getenv("INPUT_OPENAPI-DOC-LOCATION"))
+	fmt.Println(os.Getenv("INPUT_GITHUB-ACCESS-TOKEN"))
 	fmt.Println(os.Getenv("INPUT_LANGUAGES"))
 }
