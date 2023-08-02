660f77a96b8a090c208a73df1272a02abf320eb1
anuraagnalluri <anuraagnalluri@gmail.com>
2023-08-02 12:50:13 -0400

    multiline cli output

--- internal/actions/outputs.go
+++ internal/actions/outputs.go
@@ -1,7 +1,9 @@
 package actions
 
 import (
+	"encoding/base64"
 	"fmt"
+	"golang.org/x/exp/rand"
 	"os"
 
 	"github.com/speakeasy-api/sdk-generation-action/internal/logging"
@@ -19,13 +21,35 @@ func setOutputs(outputs map[string]string) error {
 	defer f.Close()
 
 	for k, v := range outputs {
-		out := fmt.Sprintf("%s=%s\n", k, v)
-		fmt.Print(out)
-
-		if _, err := f.WriteString(out); err != nil {
-			return fmt.Errorf("error writing output: %w", err)
+		if k == "cli_output" {
+			delimiter := randomDelimiter()
+			err = printAndWriteString(f, fmt.Sprintf("%s<<%s\n%s\n%s", k, delimiter, v, delimiter))
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		err = printAndWriteString(f, fmt.Sprintf("%s=%s\n", k, v))
+		if err != nil {
+			return err
 		}
 	}
 
 	return nil
+}
+
+func printAndWriteString(f *os.File, out string) error {
+	fmt.Print(out)
+	if _, err := f.WriteString(out); err != nil {
+		return fmt.Errorf("error writing output: %w", err)
+	}
+}
+
+func randomDelimiter() string {
+	b := make([]byte, 15)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(b)
 }
