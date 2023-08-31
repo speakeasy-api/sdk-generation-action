a8ac3c32cc02b00b5d4169ab17dea51fcd5102d8
anuraagnalluri <anuraagnalluri@gmail.com>
2023-08-31 12:46:04 -0700

    print commit SHA

--- internal/git/git.go
+++ internal/git/git.go
@@ -481,6 +481,8 @@ func (g *Git) WritePRComment(prNumber *int, fileName, body string, line int) err
 		return fmt.Errorf("failed to get PR: %w", err)
 	}
 
+	fmt.Println("commit SHA: ", pr.GetHead().SHA)
+
 	_, _, err = g.client.PullRequests.CreateComment(context.Background(), os.Getenv("GITHUB_REPOSITORY_OWNER"), getRepo(), *prNumber, &github.PullRequestComment{
 		Body:     github.String(sanitizeExplanations(body)),
 		Line:     github.Int(line),
