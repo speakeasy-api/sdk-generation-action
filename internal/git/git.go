19bd0299e14d329e8d67d7a091dcee3c62200dec
anuraagnalluri <anuraagnalluri@gmail.com>
2023-08-31 16:26:51 -0700

    print err

--- internal/git/git.go
+++ internal/git/git.go
@@ -531,6 +531,8 @@ func (g *Git) WritePRComment(prNumber *int, fileName, body string, line int) err
 
 	_, err = c.Do(req)
 	if err != nil {
+		fmt.Println(fmt.Sprintf("error is:  %v", err.Error()))
+		fmt.Println("error obj: ", err)
 		return err
 	}
 
