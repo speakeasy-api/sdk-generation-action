6c2b9b5264a9f261017160d72de4f838a6f0e0fb
anuraagnalluri <anuraagnalluri@gmail.com>
2023-09-05 13:38:19 -0700

    feat: add error, suggestion, and explanation as inline PR comments (#55)

--- internal/suggestions/suggestion.go
+++ internal/suggestions/suggestion.go
@@ -5,12 +5,23 @@ import (
 	"github.com/speakeasy-api/sdk-generation-action/internal/cli"
 	"github.com/speakeasy-api/sdk-generation-action/internal/environment"
 	"github.com/speakeasy-api/sdk-generation-action/internal/git"
+	"github.com/speakeasy-api/sdk-generation-action/internal/logging"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
-type prBodyInfo struct {
+var (
+	fileNameRegex      = regexp.MustCompile(`^(.*?(\.yaml|\.yml|\.json))`)
+	outputFileRegex    = regexp.MustCompile(`Suggestions applied and written to (.+)`)
+	validationErrRegex = regexp.MustCompile(`(validation (hint|warn|error):)\s+\[line (\d+)\]\s+(.*)$`)
+)
+
+type prCommentsInfo struct {
 	suggestions  []string
 	explanations []string
+	errs         []string
+	lineNums     []int
 }
 
 func Suggest(docPath, maxSuggestions string) (string, error) {
@@ -21,12 +32,23 @@ func Suggest(docPath, maxSuggestions string) (string, error) {
 	return out, nil
 }
 
-func WriteSuggestions(g *git.Git, prNumber *int, out string) error {
-	body := parseOutput(out)
-	output := formatSuggestionsAndExplanations(body)
-	if len(output) > 0 {
-		// Writes suggestions and explanations in PR body
-		if err := g.WritePRBody(prNumber, output); err != nil {
+func WriteSuggestions(g *git.Git, prNumber int, out string) error {
+	commentsInfo, fileName := parseSuggestOutput(out)
+
+	for i := 0; i < len(commentsInfo.lineNums); i++ {
+		if commentsInfo.lineNums[i] != 0 {
+			comment := formatComment(commentsInfo.errs[i], commentsInfo.suggestions[i], commentsInfo.explanations[i], 0, i+1)
+			if err := g.WritePRComment(prNumber, fileName, comment, commentsInfo.lineNums[i]); err != nil {
+				logging.Info(fmt.Sprintf("failed to write PR comment: %s", err.Error()))
+				commentsInfo.lineNums[i] = 0
+			}
+		}
+	}
+
+	body := formatBody(commentsInfo)
+	if body != "" {
+		// Writes suggestions and explanations with line number 0 PR body
+		if err := g.WritePRBody(prNumber, body); err != nil {
 			return fmt.Errorf("error writing PR body: %w", err)
 		}
 	}
@@ -34,36 +56,82 @@ func WriteSuggestions(g *git.Git, prNumber *int, out string) error {
 	return nil
 }
 
-func formatSuggestionsAndExplanations(body prBodyInfo) string {
-	var output string
-	for i := 0; i < len(body.suggestions); i++ {
-		output += fmt.Sprintf("**Suggestion %d**: %s\n\n", i+1, body.suggestions[i])
-		output += fmt.Sprintf("**Explanation %d**: %s\n\n", i+1, body.explanations[i])
+func formatComment(err, suggestion, explanation string, lineNum, index int) string {
+	var commentParts []string
+	commentParts = append(commentParts, fmt.Sprintf("**Error %d**: %s", index, err))
+
+	suggestion = fmt.Sprintf("**Suggestion %d**: %s", index, suggestion)
+	if lineNum != 0 {
+		suggestion = fmt.Sprintf("*Applied around line %d*\n%s", lineNum, suggestion)
 	}
-	return output
+	commentParts = append(commentParts, suggestion)
+	commentParts = append(commentParts, fmt.Sprintf("**Explanation %d**: %s", index, explanation))
+	return strings.Join(commentParts, "\n\n")
 }
 
-func parseOutput(out string) prBodyInfo {
-	var info prBodyInfo
+func formatBody(info prCommentsInfo) string {
+	var bodyParts []string
+	for i := 0; i < len(info.lineNums); i++ {
+		if info.lineNums[i] == 0 {
+			bodyParts = append(bodyParts, formatComment(info.errs[i], info.suggestions[i], info.explanations[i], info.lineNums[i], i+1))
+		}
+	}
+	return strings.Join(bodyParts, "\n\n")
+}
+
+func parseSuggestOutput(out string) (prCommentsInfo, string) {
+	var info prCommentsInfo
+	var lineNum int
+	var err error
 	lines := strings.Split(out, "\n")
-	suggestion, explanation := "", ""
+	suggestion, explanation, validationErr, fileName := "", "", "", ""
 	isSuggestion, isExplanation := false, false
 
 	for _, line := range lines {
+		validationErrMatch := validationErrRegex.FindStringSubmatch(line)
+		if len(validationErrMatch) == 5 {
+			lineNum, err = strconv.Atoi(validationErrMatch[3])
+			if err != nil {
+				// line number 0 indicates adding this validation error, suggestion, and explanation to PR body
+				lineNum = 0
+			}
+			validationErr = validationErrMatch[4]
+			continue
+		}
+
 		if strings.Contains(line, "Suggestion:") {
 			isSuggestion = true
 			if strings.TrimSpace(suggestion) != "" {
 				info.suggestions = append(info.suggestions, suggestion)
 			}
-			suggestion = ""
+			if strings.TrimSpace(validationErr) != "" {
+				info.errs = append(info.errs, validationErr)
+			}
+			info.lineNums = append(info.lineNums, lineNum)
+
+			suggestion, validationErr = "", ""
+			lineNum = 0
 			continue
-		} else if strings.Contains(line, "Explanation:") {
+		}
+
+		if strings.Contains(line, "Explanation:") {
 			isSuggestion = false
 			isExplanation = true
 			if strings.TrimSpace(explanation) != "" {
 				info.explanations = append(info.explanations, explanation)
 			}
 			explanation = ""
+			continue
+		}
+
+		outputFileMatch := outputFileRegex.FindStringSubmatch(line)
+		if len(outputFileMatch) == 2 {
+			// Remove leading ./ and anything trailing after file suffix
+			outputFileName := strings.Replace(outputFileMatch[1], "./", "", 1)
+			fileNameMatch := fileNameRegex.FindStringSubmatch(outputFileName)
+			if len(fileNameMatch) > 0 {
+				fileName = fileNameMatch[0]
+			}
 			continue
 		}
 
@@ -86,5 +154,5 @@ func parseOutput(out string) prBodyInfo {
 		info.explanations = append(info.explanations, explanation)
 	}
 
-	return info
+	return info, fileName
 }
