757c87da191cf4e3888594210ec41dd02a49010c
Ryan Albert <ryantimalbert@gmail.com>
2024-02-22 17:25:32 -0800

    feat: usage warning in SDK generation action

--- internal/cli/speakeasy.go
+++ internal/cli/speakeasy.go
@@ -187,9 +187,8 @@ func extractTarGZ(archive, dest string) error {
 }
 
 func CheckFreeUsageAccess() (bool, error) {
-	apiURL := "https://example.com/v1/workspace/access?passive=true"
-
-	// Create a new request using http.NewRequest
+	apiURL := "https://api.speakeasyapi.dev/v1/workspace/access?passive=true"
+	
 	req, err := http.NewRequest("GET", apiURL, nil)
 	if err != nil {
 		return false, fmt.Errorf("error creating the request: %w", err)
