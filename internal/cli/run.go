009a26bd27f1989831c87aca50b1eebab392964a
Ian Bentley <ian@idbentley.com>
2025-10-02 09:13:21 -0400

    iter

--- internal/cli/run.go
+++ internal/cli/run.go
@@ -88,7 +88,9 @@ func Run(sourcesOnly bool, installationURLs map[string]string, repoURL string, r
 		return nil, fmt.Errorf("error closing change summary file: %w", err)
 	}
 
+	fmt.Println("Before")
 	out, err := runSpeakeasyCommand(args...)
+	fmt.Println("After")
 	if err != nil {
 		return nil, fmt.Errorf("error running workflow: %w - %s", err, out)
 	}
